129a470670c96647e475afea91402b782d0086eb
SergeyYemelin <Mettlecrushit3732@gmail.com>
2024-11-22 20:56:16 +0500

    first commit

--- TimerHW/SecondViewController.swift
+++ TimerHW/SecondViewController.swift
@@ -8,22 +8,73 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var labelDate1: UILabel!
+    @IBOutlet weak var labelDate2: UILabel!
+    @IBOutlet weak var labelDateTimer: UILabel!
+  
+    var date1: Date?
+    var date2: Date?
+    
+    var timer = Timer()
+    
+    var timerRunning = false
+    
+    var savedDate: Date?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        savedDate = date1
+        labelDate1.text = dateToString(date: date1!)
+        labelDate2.text = dateToString(date: date2!)
+        labelDateTimer.text = dateToString(date: date1!)
         // Do any additional setup after loading the view.
     }
+  
+    func dateToString( date: Date ) -> String {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy, EEE HH:mm"
+        return formatter.string(from: date)
+        
+        }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func updateTimer() {
+        
+        let firstDate = date1!
+        let secondDate = date2!
+        
+        if firstDate > secondDate {
+            timer.invalidate()
+        } else {
+            if let updateDate = Calendar.current.date(byAdding: .hour, value: 1, to: date1!) {
+                date1 = updateDate
+                labelDateTimer.text = dateToString(date: updateDate)
+                
+            }
+        }
     }
-    */
-
+        
+    
+    @IBAction func starTimer(_ sender: Any) {
+        if timerRunning {
+            return
+        } else {
+            timerRunning = true
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    @IBAction func stopTimer(_ sender: Any) {
+        timer.invalidate()
+        timerRunning = false
+    }
+    
+    @IBAction func restartTimer(_ sender: Any) {
+        timer.invalidate()
+        timerRunning = false
+        
+        labelDateTimer.text = dateToString(date: savedDate!)
+    }
+    
 }
--- TimerHW/ViewController.swift
+++ TimerHW/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker1: UIDatePicker!
+    
+    @IBOutlet weak var datePicker2: UIDatePicker!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SecondVC" {
+            if let vc = segue.destination as? SecondViewController {
+                
+                vc.date1 = datePicker1.date
+                vc.date2 = datePicker2.date
+            }
+        }
+    }
 
 }
 
